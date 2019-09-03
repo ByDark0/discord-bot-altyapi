280408129893b8b18172741b0fd07aa6961528a1
ByDark <53751652+ByDark0@users.noreply.github.com>
2019-09-03 13:28:10 +0300

    Create haftalik.js

---
+++ komutlar/haftalik.js
@@ -0,0 +1,46 @@
+const db = require('quick.db')
+const ms = require('parse-ms')
+const Discord = require('discord.js')
+
+exports.run = async (client, message, args, config) => {
+
+
+    let timeout = 604800000 // 7 Milisaniye cinsinden gün, isterseniz değiştirin.
+    let amount = 1000
+    // rastgele miktar: Math.floor(Math.random() * 1000) + 1;
+
+
+    let weekly = await db.fetch(`weekly_${message.author.id}`);
+
+    if (weekly !== null && timeout - (Date.now() - weekly) > 0) {
+        let time = ms(timeout - (Date.now() - weekly));
+
+        message.channel.send(`Zaten haftalık ödülünü topladın, geri gelip toplayabilirsin **${time.days}d ${time.hours}h ${time.minutes}m ${time.seconds}s**!`)
+    } else {
+    let embed = new Discord.RichEmbed()
+    .setAuthor(`Daily`, message.author.displayAvatarURL)
+    .setColor("GREEN")
+    .setDescription(`**Haftalık Ödül**`)
+    .addField(`Toplanmış`, amount)
+
+    message.channel.send(embed)
+    db.add(`money_${message.author.id}`, amount)
+    db.set(`weekly_${message.author.id}`, Date.now())
+        
+    }
+
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["kaçıklama","kanala"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'haftalık',
+  description: '**Bulunduğunuz** kanalın konusunu/açıklamasını değiştirir. ',
+  usage: 'kanalkonusudeğiş yeni kanal ismi'
+};
+
+RealCode dan alıntıdır oraya da gidin #kod-aldığımız-yerler 
